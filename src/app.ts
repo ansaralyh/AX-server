f0c53deb1c779c1166b36cc97dece925650f04aa
Ansar Ali <ansaralyh@gmail.com>
2025-04-24 05:03:22 +0500

    fileupload error resolved

--- src/app.ts
+++ src/app.ts
@@ -32,10 +32,10 @@ app.use(cookieParser());
 app.use(morgan("dev"));
 
 // Serve static files from uploads directory
-app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+app.use('/uploads', express.static(path.join(__dirname, '../uploads/driver-documents')));
 
 // Security headers
-app.use((req, res, next) => {
+app.use((req, res, next) => {       
   res.setHeader('X-Content-Type-Options', 'nosniff');
   res.setHeader('X-Frame-Options', 'DENY');
   res.setHeader('X-XSS-Protection', '1; mode=block');
